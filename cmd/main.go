ac94aade5e5a5c0999526c7fed52fb160e07a071
bambutcha <stringdeity@gmail.com>
2025-02-07 23:29:18 +0300

    Fix problems
    [#ideas]

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/bambutcha/cs2-bhop/internal/app/bhop"
 	"github.com/bambutcha/cs2-bhop/internal/app/logger"
 )
@@ -9,4 +11,7 @@ func main() {
 	logger := logger.NewLogger()
 	bhop := bhop.NewBhop(logger)
 	bhop.Start()
+	
+	fmt.Println("\nНажмите Enter для выхода...")
+	fmt.Scanln()
 }
